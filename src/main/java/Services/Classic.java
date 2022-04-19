d4569d95509f3901493ac52c4c1bfdc73968d1f0
MaximoMag <maxmageiro@gmail.com>
2022-04-19 15:41:32 -0300

    Servicion (revisar: orderValue / cantidades ilimitadas)

---
+++ src/main/java/Services/Classic.java
@@ -0,0 +1,12 @@
+package Services;
+
+public class Classic extends Service {
+
+    public Classic(){
+        this.cleaningQuantity = -1;
+        this.orderingQuantity = 3;
+        this.orderValue = 0;
+        this.maxDebt = 2000;
+    }
+    
+}
