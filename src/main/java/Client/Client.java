99358be265c114e45a9488e5efabfea9a4e470a7
Luciano Ponce <poncelucianod@gmail.com>
2022-05-02 17:39:10 -0300

    Se agrega clase Cliente

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -1,4 +1,23 @@
 package Client;
+import Service.*;
 
 public class Client {
+    private int dni;
+    private Service service;
+    private float debt;
+
+    public Client(){
+    }
+
+    public OrderPending requestOrder(Company company, CleanType cleanType, Location location) throws CouldNotCreateOrderException{
+        try{
+            OrderPending order = new OrderPending();
+            order = company.createOrderPending(CleanType cleanType, Location location);
+            return order;
+        }
+        catch (Exception CouldNotCreateOrderException){
+            throw new CouldNotCreateOrderException();
+        }
+
+    }
 }
