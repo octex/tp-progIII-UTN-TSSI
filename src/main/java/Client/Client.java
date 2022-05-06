3f8eb276f653b3c22f6296991564f5ebd0f8cfc4
Luciano Ponce <poncelucianod@gmail.com>
2022-05-06 19:26:07 -0300

    Agrego Diagrama V2

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -1,16 +1,16 @@
 package Client;
+
 import Service.*;
+import Order.Order;
 
 public class Client {
     private int dni;
     private float debt;
     private Service service;
-    private RequestedOrder requestedOrder;
+    private Order requestedOrder;
 
-    public Client(){
-    }
 
-    public OrderPending requestOrder(Company company, CleanType cleanType, Location location) throws CouldNotCreateOrderException{
+    public Order requestOrder(Company company, CleanType cleanType, Location location) throws CouldNotCreateOrderException{
         try{
             OrderPending order = new OrderPending();
             order = company.createOrderPending(CleanType cleanType, Location location);
