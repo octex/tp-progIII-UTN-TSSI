157f5aa56c42b70f6fcd8e75df8ba3cbc730c732
Luciano Ponce <poncelucianod@gmail.com>
2022-05-09 19:40:37 -0300

    Actualizo clase Company

--- src/main/java/CompanyUtils/Company.java
+++ src/main/java/CompanyUtils/Company.java
@@ -4,6 +4,7 @@ import Client.Client;
 import Order.Order;
 import Robots.Robot;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 
@@ -11,8 +12,18 @@ public class Company {
     private ArrayList<Robot> robots;
     private ArrayList<Client> clients;
     private ArrayList<Order> orders;
-
     private AllocatorSystem allocatorSystem;
+    private LinkedList<RobotRegister> orderPerRobot;
 
+    public Company(ArrayList<Robot> robots, ArrayList<Client> clients, ArrayList<Order> orders){
+        this.robots.addAll(robots);
+        this.clients.addAll(clients);
+        this.orders.addAll(orders);
+        this.allocatorSystem = new AllocatorSystem();
+        this.orderPerRobot = new LinkedList();
+    }
 
+    public void tryToAssing(Order order){
+        this.allocatorSystem.TryAllocate(order);
+    }
 }
