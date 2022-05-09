57703a746228008366cbc2df8a92ad8c5b6855ee
octex <56984557+octex@users.noreply.github.com>
2022-05-09 01:53:11 -0300

    Refactor en Client

--- src/main/java/Client/Classic.java
+++ src/main/java/Client/Classic.java
@@ -1,23 +1,9 @@
 package Client;
 
-import Order.*;
-import CompanyUtils.Company;
-
 
 public abstract class Classic extends Client {
 
-    public void Client(){
-    }
-
-    public Order requestOrder(Company company, CleanType cleanType, Location location, boolean wantsOrder) throws CouldNotCreateOrderException{
-        try{
-            Order order = new Order(cleanType, location, wantsOrder);
-            order = company.createOrderPending(CleanType cleanType, Location location);
-            return order;
-        }
-        catch (Exception CouldNotCreateOrderException){
-            throw new CouldNotCreateOrderException("No se pudo crear la orden");
-        }
-
+    public Classic()
+    {
     }
 }
--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -1,10 +1,23 @@
 package Client;
 
-import Order.Order;
+import CompanyUtils.Company;
+import Order.*;
 
 public class Client {
     private int dni;
     private float debt;
     private Services.Service service;
     private Order requestedOrder;
+
+    public Order requestOrder(Company company, CleanType cleanType, Location location, boolean wantsOrder) throws CouldNotCreateOrderException{
+        try{
+            Order order = new Order(cleanType, location, wantsOrder);
+            order = company.createOrderPending(CleanType cleanType, Location location); // Metodo sin definir
+            return order;
+        }
+        catch (Exception CouldNotCreateOrderException){
+            throw new CouldNotCreateOrderException("No se pudo crear la orden");
+        }
+
+    }
 }
