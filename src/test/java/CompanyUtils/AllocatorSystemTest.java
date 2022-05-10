5a5362db8807f26361a55737aeab9e6f9b88ff32
octex <56984557+octex@users.noreply.github.com>
2022-05-10 17:32:32 -0300

    Created first test

---
+++ src/test/java/CompanyUtils/AllocatorSystemTest.java
@@ -0,0 +1,17 @@
+package CompanyUtils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AllocatorSystemTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void tryAllocate() {
+    }
+}
