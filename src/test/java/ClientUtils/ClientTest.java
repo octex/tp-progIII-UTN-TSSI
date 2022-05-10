0c11038427aecd959eadee43474a032695f59003
MaximoMag <maxmageiro@gmail.com>
2022-05-10 18:16:48 -0300

    enum error

--- src/test/java/ClientUtils/ClientTest.java
+++ src/test/java/ClientUtils/ClientTest.java
@@ -2,8 +2,14 @@ package ClientUtils;
 
 import java.beans.Transient;
 
-import Client.CouldNotCreateOrderException;
-import org.junit.jupiter.api.Test;
+import CompanyUtils.AllocatorSystemExeptions.ServiceNotIncludedExeption;
+import Client.Client;
+import CompanyUtils.Company;
+import Order.CleanType;
+import Order.Order;
+import Order.CleanType;
+
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,8 +22,8 @@ public class ClientTest {
         void economicLimpiezaOrdenamiento(){
             var client = new Client(111, new Economic(), null);
 
-            assertThrows(CouldNotCreateOrderException.class, ()->{
-                         client.requestOrder(new Company(), new Order(SIMPLE, null, true, "a"));
+            assertThrows(ServiceNotIncludedExeption.class, ()->{
+                         client.requestOrder(new Company(), new Order(client, SIMPLE, null, true, "a"));
             });
         }
 
