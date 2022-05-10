262a5f94e1a51d5b63edc82dcf32bec050823083
MaximoMag <maxmageiro@gmail.com>
2022-05-10 15:36:28 -0300

    Correccion, cantidad de ordenamientos de economic

--- src/main/java/Services/Economic.java
+++ src/main/java/Services/Economic.java
@@ -4,7 +4,7 @@ public class Economic extends Service {
 
     public Economic(){
         this.cleaningQuantity = 3;
-        this.orderingQuantity = 3;
+        this.orderingQuantity = -1;
         this.orderValue = 0;
         this.maxDebt = 0;
     }
