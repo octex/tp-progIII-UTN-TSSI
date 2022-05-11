07779bc7b13fa6f7e84ff3dc7bbee8bedee3b94c
octex <56984557+octex@users.noreply.github.com>
2022-05-10 23:06:40 -0300

    Se aniade un mock para el modulo de pagos. Casos planteados para robotassigner

--- src/test/java/CompanyUtils/RobotAssignerTest.java
+++ src/test/java/CompanyUtils/RobotAssignerTest.java
@@ -7,12 +7,54 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RobotAssignerTest {
 
+    public RobotAssigner robotAssigner;
+    public PaymentModuleMock mock;
     @BeforeEach
-    void setUp() {
+    void setUp()
+    {
+        robotAssigner = new RobotAssigner();
+        mock = new PaymentModuleMock();
     }
 
     @Test
-    void tryAllocate() {
+    void AssignRobotForEconomicService()
+    {
+
     }
 
+    @Test
+    void AssignRobotForClassicService()
+    {
+
+    }
+
+    @Test
+    void AssignRobotForPlatinumClient()
+    {
+
+    }
+
+    @Test
+    void AssignRobotForEconomicServiceWithDebt()
+    {
+
+    }
+
+    @Test
+    void AssignRobotForClassicServiceWithDebt()
+    {
+
+    }
+
+    @Test
+    void AssignRobotForClassicServiceWithExcededDebt()
+    {
+
+    }
+
+    @Test
+    void AssignRobotForPlatinumServiceWithExcededDebt()
+    {
+
+    }
 }
