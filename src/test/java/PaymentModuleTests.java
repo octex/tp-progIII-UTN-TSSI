778290be788d315b2aa1195ff8d4503662acb84e
bmirocznyk <bruno.mirocznyk@emeal.nttdata.com>
2022-05-10 22:43:10 -0300

    TestsPruebaVerify

--- src/test/java/PaymentModuleTests.java
+++ src/test/java/PaymentModuleTests.java
@@ -1,11 +1,11 @@
-package src.test.java;
+
 
 import CompanyUtils.PaymentModule;
 
 public class PaymentModuleTests {
 
     private PaymentModule paymentModule;
-    paymentModule = new PaymentModule();
+    //paymentModule = new PaymentModule();
 
 
 }
