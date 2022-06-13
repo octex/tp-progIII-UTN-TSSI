431e35ed7a01eaa6d76d50cfdc5ac307ddde873b
MaximoMag <maxmageiro@gmail.com>
2022-06-13 18:28:42 -0300

    simple clean correction

--- src/main/java/Order/FactoryCleanType/SimpleClean.java
+++ src/main/java/Order/FactoryCleanType/SimpleClean.java
@@ -4,7 +4,16 @@ import Order.Order;
 
 public class SimpleClean implements CleanType{
     @Override
-    public Float calculatePrice(Order order) {
-        return null;
+
+    public Float calculatePrice(Order order){
+
+        float sumatory = 0;
+
+        order.getRobots().stream().forEach(y -> sumatory += y.getCostPH());
+
+        sumatory += (order.getEmpleado().getSalary / 160) * order.getReparation().complexity;
+
+        sumatory += order.getReparation().cost;
+
+        return sumatory;
     }
-}
