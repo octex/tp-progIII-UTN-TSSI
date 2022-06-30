74fe9cb44d0606350bcba3d6410b078f38342943
bmirocznyk <bruno.mirocznyk@emeal.nttdata.com>
2022-06-29 22:44:04 -0300

    Client Orders Company Registry

---
+++ src/main/java/CompanyUtils/ClientOrders.java
@@ -0,0 +1,44 @@
+package CompanyUtils;
+
+import Client.Client;
+import Order.Order;
+
+import java.util.ArrayList;
+
+public class ClientOrders {
+    Client client;
+    ArrayList<Order> clientOrders;
+
+    public ClientOrders(Client client, ArrayList<Order> clientOrders) {
+        this.client = client;
+        this.clientOrders = clientOrders;
+    }
+
+    float calculateClientTotal(){
+
+        float sum = 0;
+        for(int i = 0; i < clientOrders.size(); i++){
+            sum += clientOrders.get(i).getOrderPrice();
+
+    }
+        return sum;
+
+    }
+
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public ArrayList<Order> getClientOrders() {
+        return clientOrders;
+    }
+
+    public void setClientOrders(ArrayList<Order> clientOrders) {
+        this.clientOrders = clientOrders;
+    }
+}
